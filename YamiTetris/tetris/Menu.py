aa3c115e86a3b75942d4532f74e6a36cf040cbf9
KimYutan <yutan0565>
2020-12-06 22:25:40 +0900

    mid

--- YamiTetris/tetris/Menu.py
+++ YamiTetris/tetris/Menu.py
@@ -5,10 +5,10 @@ from Tetris import *
 from Database import *
 
 menu_image = pygame_menu.baseimage.BaseImage(
-    image_path='assets/images/메인메뉴2.PNG',
+    image_path='assets/images/메인메뉴2.png',
     drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL	)
 widget_image = pygame_menu.baseimage.BaseImage(
-    image_path='assets/images/메인위젯.PNG',
+    image_path='assets/images/메인위젯.png',
     drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL)
 
 mytheme=pygame_menu.themes.THEME_ORANGE.copy()
